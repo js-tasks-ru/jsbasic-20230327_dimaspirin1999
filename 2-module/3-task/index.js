ee1883088742df670db60b120d6d23b9fe0b3f85
Dmitry Spirin <dimaspirin1999@mail.ru>
2023-04-10 21:37:40 +0300

    исправил 3 задачу во 2 модуле

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,11 +1,19 @@
-const calculator = {
-  read: (a, b) => {
+let calculator = {
+  read: function (a, b) {
     this.a = a;
     this.b = b;
   },
-  sum: () => this.a + this.b,
-  mul: () => this.a * this.b,
+    
+  sum: function () { 
+    return this.a + this.b; 
+  },
+  
+  mul: function () {
+    return this.a * this.b;
+  },
 };
+
+
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
