1c587e007ebe1f7419db563cd67b6ec3b452256c
Dmitry Spirin <dimaspirin1999@mail.ru>
2023-04-15 23:19:08 +0300

    5 задача 3 модуля

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,13 @@
 function getMinMax(str) {
-  // ваш код...
+  let num = str.split(/[ ,]+/);
+   
+  let minV = +num[0];
+  let maxV = minV;
+    for (let i = 0; i < num.length; i++) {
+      let e = +num[i];
+      if (e < minV) minV = e;
+      if (e > maxV) maxV = e;
+    }
+    return {min: minV, max: maxV};
 }
+  
